0b451bb396d8e007faa9fc386709dd495432a951
Julia Hartmann <juli.hartmann@bluewin.ch>
2017-12-12 18:38:53 +0100

    Fix SocrativeController. Now it saves properly.

--- grails-app/controllers/mvc/SocrativeController.groovy
+++ grails-app/controllers/mvc/SocrativeController.groovy
@@ -20,18 +20,18 @@ class SocrativeController {
 
     def save () {
         Questionblock qb = new Questionblock(name: params.get("name"), numberOfQuestions: 1 )
-        qb.save()
-        Answer a1 = new Answer(answer: params.get("answer")[0], correct: params.get("answer1correct"))
-        a1.save()
-        Answer a2 = new Answer(answer: params.get("answer")[1], correct: params.get("answer2correct"))
-        a2.save()
-        Answer a3 = new Answer(answer: params.get("answer")[2], correct: params.get("answer3correct"))
-        a3.save()
-        Answer a4 = new Answer(answer: params.get("answer")[3], correct: params.get("answer4correct"))
-        a4.save()
+        qb.save(flush: true, failOnError: true)
+        Answer a1 = new Answer(answer: params.get("answer")[0], correct: (params.get("answer1correct")?true:false))
+        a1.save(flush: true, failOnError: true)
+        Answer a2 = new Answer(answer: params.get("answer")[1], correct: (params.get("answer2correct")?true:false))
+        a2.save(flush: true, failOnError: true)
+        Answer a3 = new Answer(answer: params.get("answer")[2], correct: (params.get("answer3correct")?true:false))
+        a3.save(flush: true, failOnError: true)
+        Answer a4 = new Answer(answer: params.get("answer")[3], correct: (params.get("answer4correct")?true:false))
+        a4.save(flush: true, failOnError: true)
 
         Question q = new Question(questionblock: qb, question: params.get("question"), answer1: a1, answer2: a2, answer3: a3, answer4: a4)
-        q.save()
+        q.save(flush: true, failOnError: true)
 
         if(params.containsKey('save')){
             //index()
@@ -64,6 +64,20 @@ class SocrativeController {
         } else if(params.containsKey('addmore')) {
             render view:"addMoreQuestionsView", model:[title:qb.name, id:qb.id]
         }
+    }
+
+    def getQuestions(){
+        List<Questionblock> qbs = Questionblock.all
+        List<Question> qs = Question.all
+        List<Answer> ans = Answer.all
+
+
+        Questionblock questionblock = Questionblock.get(Integer.parseInt(params.get("questionblockID").toString()))
+        println(questionblock)
+        List<Question> a = Question.all
+        List<Question> questions = Question.findByQuestionblock(questionblock) as List<Question>
+        println(questions)
+        render view:"quizView", model:[questions: questions]
     }
 
 
---
+++ grails-app/views/socrative/quizView.gsp
@@ -0,0 +1,10 @@
+<g:each var="question" in="${questions}">
+    <p>
+        ${question.question}
+        ${question.answer1}
+        ${question.answer2}
+        ${question.answer3}
+        ${question.answer4}
+    </p>
+</g:each>
+test
